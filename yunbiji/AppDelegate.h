3776b3364642549259dc8c2d80a11dc19af87c8e
guo <389878212@qq.com>
2013-09-22 19:00:44 +0800

    Initial Commit

---
+++ yunbiji/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  yunbiji
+//
+//  Created by admin on 13-9-22.
+//  Copyright (c) 2013年 cocoa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
